af1d5b232fe1c303101556070f3191f018a2a118
joao-lusa <joaosombrio@rede.ulbra.br>
2022-07-01 13:33:10 -0300

    Initial Commit

---
+++ lib/app_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:payflow/modules/home/home_page.dart';
+
+import 'shared/themes/app_colors.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
+      home: HomePage(),
+      initialRoute: "/home",
+      routes: {
+        "/home": (context) => HomePage(),
+        "/barcode_scanner": (context) => BarcodeScannerPage()
+      },
+    );
+  }
+}
